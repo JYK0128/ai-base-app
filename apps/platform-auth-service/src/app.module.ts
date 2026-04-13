3ecf35f73ea3a1d324afc848ac65ad5071dbff87
jyk0128 <wlsyong90@gmail.com>
2026-04-13 10:05:03 +0900

    dev. platform-auth 센츄리 장착

--- apps/platform-auth-service/src/app.module.ts
+++ apps/platform-auth-service/src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { TerminusModule } from '@nestjs/terminus';
+import { SentryGlobalFilter, SentryModule } from '@sentry/nestjs/setup';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [TerminusModule],
+  imports: [
+    SentryModule.forRoot(),
+    TerminusModule,
+  ],
   controllers: [AppController, HealthController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: SentryGlobalFilter,
+    },
+  ],
 })
 export class AppModule {}
