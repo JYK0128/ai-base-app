716a2335c6f9f446505dac7e3b7c936bc288c2cf
jyk0128 <wlsyong90@gmail.com>
2026-04-17 10:59:27 +0900

    dev. seeding 추가

---
+++ packages/database/src/seeders/AuthSeeder.ts
@@ -0,0 +1,33 @@
+import { randomUUID } from 'node:crypto';
+
+import type { EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+
+import { ManagerAccount } from '@/domains/platform/manager/manager.account.entity';
+
+const managerAccountSeeds = [
+  {
+    email: 'test@example.com',
+    password: 'pass1234',
+  },
+] as const;
+
+export class AuthSeeder extends Seeder {
+  async run(em: EntityManager): Promise<void> {
+    for (const managerAccountSeed of managerAccountSeeds) {
+      const found = await em.findOne(ManagerAccount, { email: managerAccountSeed.email });
+
+      if (!found) {
+        em.persist(em.create(ManagerAccount, {
+          id: randomUUID(),
+          ...managerAccountSeed,
+        }));
+        continue;
+      }
+
+      found.password = managerAccountSeed.password;
+    }
+
+    await em.flush();
+  }
+}
--- packages/database/src/seeders/PlatformSeeder.ts
+++ packages/database/src/seeders/PlatformSeeder.ts
@@ -1,6 +1,7 @@
 import type { EntityManager } from '@mikro-orm/core';
 import { Seeder } from '@mikro-orm/seeder';
 
+import { AuthSeeder } from './AuthSeeder';
 import { MessageSeeder } from './MessageSeeder';
 import { OrganizationSeeder } from './OrganizationSeeder';
 import { RbacSeeder } from './RbacSeeder';
@@ -8,6 +9,6 @@ import { SiteSeeder } from './SiteSeeder';
 
 export class PlatformSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
-    await this.call(em, [OrganizationSeeder, SiteSeeder, RbacSeeder, MessageSeeder]);
+    await this.call(em, [OrganizationSeeder, SiteSeeder, RbacSeeder, MessageSeeder, AuthSeeder]);
   }
 }
